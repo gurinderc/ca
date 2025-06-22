6b195e27251b2ac8b5088a4d4a81a25dbe09aeed
Gurinder Chhabra <1164603+gurinderc@users.noreply.github.com>
2025-06-22 18:33:12 +0100

    Create bookmarks.js

---
+++ bookmarks.js
@@ -0,0 +1,36 @@
+function getBookmarks() {
+	// For demo purposes, return sample data
+	return {
+		laps: [
+			{ title: "Passwords", url: "https://pass.gurinder.net" }
+		],
+		transport: [
+			{ title: "NEH", url: "http://ojp.nationalrail.co.uk/service/ldbboard/dep/NEH" },
+			{ title: "CST", url: "http://ojp.nationalrail.co.uk/service/ldbboard/dep/CST" },
+			{ title: "CHX", url: "http://ojp.nationalrail.co.uk/service/ldbboard/dep/CHX" },
+			{ title: "LBG", url: "http://ojp.nationalrail.co.uk/service/ldbboard/dep/LBG" },
+			{ title: "HGR", url: "http://ojp.nationalrail.co.uk/service/ldbboard/dep/HGR" },
+			{ title: "LEW", url: "http://ojp.nationalrail.co.uk/service/ldbboard/dep/LEW" }
+		],
+		deals: [
+			{ title: "HUKD", url: "https://www.hotukdeals.com/deals-new" },
+			{ title: "HFP", url: "https://www.headforpoints.com/" },
+			{ title: "Reddit", url: "https://www.reddit.com/" }
+		],
+		news: [
+			{ title: "BBC News", url: "https://www.bbc.co.uk/news" },
+			{ title: "London Weather", url: "https://www.bbc.co.uk/weather/2643743" },
+			{ title: "Sky News", url: "https://news.sky.com/uk" },
+			{ title: "Reuters", url: "https://www.reuters.com/" },
+			{ title: "Reuters UK", url: "https://uk.reuters.com/" }
+		],
+		finance: [
+			{ title: "TradingView", url: "https://www.tradingview.com/markets/" },
+			{ title: "Yahoo Finance", url: "https://finance.yahoo.com/world-indices" },
+			{ title: "Yahoo UK", url: "https://uk.finance.yahoo.com/world-indices" },
+			{ title: "Bloomberg", url: "https://www.bloomberg.com/" },
+			{ title: "Money Control", url: "https://www.moneycontrol.com" },
+			{ title: "HL", url: "https://www.hl.co.uk/shares/stock-market-summary/ftse-100" }
+		]
+	};
+}
